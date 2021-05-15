a80fea81bd404b629d82e7642c7d1a59f5b30373
SUDIP MONDAL <sudipmondal.2002@rediffmail.com>
2021-05-15 22:03:36 +0530

    added the layout

---
+++ app/src/main/java/com/sudip/drumkitapp/MainActivity.java
@@ -0,0 +1,20 @@
+package com.sudip.drumkitapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void buttonTapped(View view)
+    {
+        Log.i("info", "buttonTapped: true ");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
